aa712cf5a6b59f4256a902744973cde7ca3e8f09
laosijink <313242815@qq.com>
2019-10-05 20:09:17 +0800

    ‘初始化版本’

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let cssnano = require('gulp-cssnano');
+let rename = require('gulp-rename');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+
+gulp.task('sass',()=>{
+    gulp.src('./src/sass/*.scss')
+    .pipe(sass())
+    .pipe(cssnano())
+    .pipe(rename({suffix : '.min'}))
+    .pipe(gulp.dest('./dist/css'));
+})
+
+gulp.task('default',()=>{
+    gulp.watch('./src/sass/*.scss',['sass']);
+})
